a051f2cd4b0e3d0dde0110592f6914fde7ae462e
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-06-29 23:42:56 -0500

    sketch out i386

--- i386/test/hello.s
+++ i386/test/hello.s
@@ -1,30 +1,34 @@
+.section .comment
+  .byte 0
+  .asciz "GCC: (GNU) 2.95.3-haiku-2017_07_20"
+
 .section .rodata
 
 msg: .ascii "Hello World!\n"
 
 .equ msg_len, .-msg
-.equ sys_write, 4
-.equ sys_exit, 1
+.equ kernel, 0x63
+.equ write, 131
+.equ exit, 8
 .equ stdout, 1
-.equ kernel, 0x80
+.equ status, 17
 
 .text
 
 .global _start
 
 _start:
-  /* TODO: Fill in valid _write, _exit ops */
-  /* ... */
+  # TODO: Fill in valid _write ops
+  # ...
 
-  push $msg_len
-  push $msg
-  push $stdout
-  push %eax
-  mov $sys_write, %eax
-  int $kernel
-  add $12, %esp
+  # push $msg_len
+  # push $msg
+  # push $stdout
+  # push %eax
+  # mov $sys_write, %eax
+  # int $kernel
+  # add $12, %esp
 
-  push $0
-  push %eax
-  mov $sys_exit, %eax
+  push $status
+  mov $exit, %eax
   int $kernel
